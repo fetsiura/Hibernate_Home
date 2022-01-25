cbd3940099ff4c08118add1fbadfa137e7980516
fetsiura <fetsiura@gmail.com>
2022-01-25 18:44:11 +0100

    some

---
+++ src/main/java/pl/jaro/Home/HomeController.java
@@ -0,0 +1,16 @@
+package pl.jaro.Home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    @ResponseBody
+    public String home (){
+
+
+    }
+}
