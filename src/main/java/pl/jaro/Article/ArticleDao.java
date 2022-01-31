a7edc206a4ae5011066495e1b415c9f586d81535
fetsiura <fetsiura@gmail.com>
2022-01-31 22:25:46 +0100

    some

--- src/main/java/pl/jaro/Article/ArticleDao.java
+++ src/main/java/pl/jaro/Article/ArticleDao.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import pl.jaro.common.GenericDao;
 
+import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ArticleDao extends GenericDao<Article> {
@@ -20,5 +22,27 @@ public class ArticleDao extends GenericDao<Article> {
         Query query = entityManager.createQuery("select a from articles a");
 
         return query.getResultList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Article> getLastFive(){
+        Query query = entityManager.createQuery("select a from articles a order by a.created desc");
+        query.setMaxResults(5);
+        return query.getResultList();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Article> findAllWithCategories (Long id){
+
+        Query query = entityManager.createQuery("select a from articles a JOIN FETCH a.categories c where a.id= :id");
+        query.setParameter("id",id);
+
+        try {
+            Article article =(Article) query.getSingleResult();
+            return Optional.of(article);
+        } catch (NoResultException ex){
+            return Optional.empty();
+        }
+
     }
 }
--- src/main/java/pl/jaro/Home/HomeController.java
+++ src/main/java/pl/jaro/Home/HomeController.java
@@ -1,16 +1,38 @@
 package pl.jaro.Home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.jaro.Article.ArticleDao;
+import pl.jaro.Category.Category;
+import pl.jaro.Category.CategoryDao;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
+    private final ArticleDao articleDao;
+    private final CategoryDao categoryDao;
+
+    public HomeController(ArticleDao articleDao, CategoryDao categoryDao) {
+        this.articleDao = articleDao;
+        this.categoryDao = categoryDao;
+    }
+
     @GetMapping
-    @ResponseBody
-    public String home (){
+    public String home (Model model){
+        model.addAttribute("articles",articleDao.getLastFive());
+        return "/home";
+    }
 
 
+    @ModelAttribute("categories")
+    public List<Category> categories(){
+        return categoryDao.findAll();
     }
 }
